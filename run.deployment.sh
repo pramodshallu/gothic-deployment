fb06aea4cd9e740a4517a6d93debb49c49a85124
pramodshallu <pramodshallu@gmail.com>
2026-05-23 10:24:47 -0400

    Update run.deployment.sh

--- run.deployment.sh
+++ run.deployment.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+echo "🔄 Syncing latest code from GitHub..."
+git pull origin main
+
+echo "🛑 Removing old container instance..."
+podman stop gothic-site 2>/dev/null || true
+podman rm gothic-site 2>/dev/null || true
+
+echo "🔨 Building Podman image..."
+podman build -t test-nginx:v2 .
+
+#echo "🚀 Starting Nginx server..."
+#podman run -d --name gothic-site -p 8082:80 test-nginx:v1
+
+#echo "✅ Success! Open http://localhost:8082 in your browser."
+
+
 # 1. Apply the manifest file to create the deployment and service
 kubectl apply -f njinx-deployment.yaml
 
